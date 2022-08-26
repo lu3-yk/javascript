a3b2b49b78f7d5aa51f32f68dea00f2e8664c3e2
wadayuki <106656539+lu3-yk@users.noreply.github.com>
2022-08-26 15:37:07 +0000

    javascript再宣言・再代入練習

---
+++ practice-variable/sample.js
@@ -0,0 +1,34 @@
+// var nickname = "taro"
+// console.log(nickname)
+// var nickname = "ichiro"
+// console.log(nickname)
+
+// let nickname = "taro"
+// console.log(nickname)
+// let nickname = "ichiro"
+// console.log(nickname)
+
+// const nickname = "taro"
+// console.log(nicname)
+// const.nickname = "ichiro"
+// console.log(nickname)
+
+// var nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+
+// let nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+
+// var str = "webcamp"
+
+// function foo(){
+//   console.log(str)
+//   var y = "hello"
+// }
+
+// foo()
+// console.log(y)
